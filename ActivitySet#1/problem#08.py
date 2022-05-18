61daa701f65959eb349e10a4a0767d7709f3c166
Abhinandan0601 <97089729+Abhinandan0601@users.noreply.github.com>
2022-05-18 16:01:06 +0000

    p8 done

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,15 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+a = open(fname)
+count = 0
+total = 0
+for line in a:
+    if line.startswith("X-DSPAM-Confidence:"):
+        count = count + 1
+        x = line.lstrip("X-DSPAM-Confidence:")
+        x = float(x)
+        total = total + x
+print("Average spam confidence:",total/count)        
+
